24b836118d4376f85f4b76e7747a68a913ce7c59
leochan007 <leochan007@163.com>
2025-12-05 01:30:03 +0800

    scaldfolding update

--- man-dash-monorepo/apps/mantle-dash-ext/popup.tsx
+++ man-dash-monorepo/apps/mantle-dash-ext/popup.tsx
@@ -11,7 +11,7 @@ function IndexPopup() {
       <h2>
         Welcome to your{" "}
         <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
+          leochan
         </a>{" "}
         Extension!
       </h2>
